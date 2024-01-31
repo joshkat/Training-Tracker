43c85a400b084e3d68f4457b573049192cfdcb44
joshkat <joshua02k@gmail.com>
2024-01-30 20:27:08 -0500

    Custom 404 - Still need to style properly, but fits better than next default 404

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="text-center">
+      <h2 className="text-3xl">There was a problem</h2>
+      <p>That page couldn&apos;t be found, sorry!</p>
+    </main>
+  );
+}
