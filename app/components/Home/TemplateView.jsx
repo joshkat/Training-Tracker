f3c1378700db4f5cc855d24f1ad2b6fc03dac5c9
Josh <joshua02k@gmail.com>
2024-02-05 21:26:25 -0500

    Minor style changes - Added title to /workouts - Fixed height of box within TemplateView

--- app/components/Home/TemplateView.jsx
+++ app/components/Home/TemplateView.jsx
@@ -11,7 +11,7 @@ export default function TemplateView({
       {templates === null ? (
         <span className="loading loading-spinner w-24 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center overflow-scroll h-[calc(100vh - (65px))] no-scrollbar">
+        <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center overflow-scroll h-[calc(100vh-(97px+1rem))] mt-[1rem] no-scrollbar">
           {templates.map(templateObj => (
             <Template
               title={templateObj.title}
